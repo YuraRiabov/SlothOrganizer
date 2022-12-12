47416efe3071197c2155521cbe9352a3c0d10120
Yurii Riabov <yurii.riabov@realplusonline.com>
2022-12-12 11:07:31 +0200

    Rename http service file

--- frontend/src/app/api/auth.service.ts
+++ frontend/src/app/api/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpService } from './http-internal.service';
+import { HttpService } from './http.service';
 import { Injectable } from '@angular/core';
 import { NewUser } from '#types/user/new-user';
 import { Observable } from 'rxjs';
