266278d0c92186612d4cc481a544eb5b3f4f2798
Yurii Riabov <yurii.riabov@realplusonline.com>
2023-01-10 12:59:26 +0200

    Add tasks displaying frontend unit tests

--- frontend/src/app/features/dashboard/task-info/task-info.component.spec.ts
+++ frontend/src/app/features/dashboard/task-info/task-info.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from '../dashboard-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@shared/material/material.module';
+import { Store } from '@ngrx/store';
+import { TaskBlock } from '#types/dashboard/timeline/task-block';
 import { TaskInfoComponent } from './task-info.component';
+import { of } from 'rxjs';
 
 describe('TaskInfoComponent', () => {
-  let component: TaskInfoComponent;
-  let fixture: ComponentFixture<TaskInfoComponent>;
+    let component: TaskInfoComponent;
+    let fixture: ComponentFixture<TaskInfoComponent>;
+    let store: jasmine.SpyObj<Store>;
+    const taskBlock: TaskBlock = {
+        task: {
+            title: 'title',
+            id: 1,
+            dashboardId: 1,
+            description: 'title',
+            taskCompletions: []
+        },
+        taskCompletion: {
+            start: new Date(2022, 11, 16, 8),
+            end: new Date(2022, 13, 1, 0),
+            id: 1,
+            taskId: 1,
+            isSuccessful: false
+        },
+        color: 'blue'
+    };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TaskInfoComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        store = jasmine.createSpyObj('Store', ['select']);
+        store.select.and.returnValue(of(taskBlock));
+        await TestBed.configureTestingModule({
+            declarations: [TaskInfoComponent],
+            imports: [
+                CommonModule,
+                DashboardRoutingModule,
+                MaterialModule,
+                FormsModule
+            ],
+            providers: [
+                { provide: Store, useValue: store }
+            ]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(TaskInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TaskInfoComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/src/app/features/dashboard/timeline/timeline.component.spec.ts
+++ frontend/src/app/features/dashboard/timeline/timeline.component.spec.ts
@@ -6,82 +6,92 @@ import { DashboardRoutingModule } from '../dashboard-routing.module';
 import { ExceedingTasksComponent } from '../exceeding-tasks/exceeding-tasks.component';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@shared/material/material.module';
-import { TaskBlock } from '#types/dashboard/timeline/task-block';
+import { Store } from '@ngrx/store';
+import { Task } from '#types/dashboard/tasks/task';
 import { TimelineComponent } from './timeline.component';
 import { TimelineScale } from '#types/dashboard/timeline/enums/timeline-scale';
+import { of } from 'rxjs';
+import { selectTasks } from '@store/selectors/dashboard.selectors';
 
 describe('TimelineComponent', () => {
     let component: TimelineComponent;
     let fixture: ComponentFixture<TimelineComponent>;
-    const mockTasks: TaskBlock[] = [
-        {
-            start: new Date(2022, 11, 16, 8),
-            end: new Date(2022, 13, 1, 0),
-            title: 'month block'
-        },
-        {
-            start: new Date(2022, 12, 16, 8),
-            end: new Date(2022, 12, 22, 0),
-            title: 'big block'
-        },
-        {
-            start: new Date(2022, 12, 16, 8),
-            end: new Date(2022, 12, 16, 11),
-            title: 'first block'
-        },
-        {
-            start: new Date(2022, 12, 16, 15),
-            end: new Date(2022, 12, 16, 17),
-            title: 'second block'
-        },
-        {
-            start: new Date(2022, 12, 16, 10),
-            end: new Date(2022, 12, 16, 23),
-            title: 'third block'
-        },
-        {
-            start: new Date(2022, 12, 16, 12),
-            end: new Date(2022, 12, 16, 18),
-            title: 'fourth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 13),
-            end: new Date(2022, 12, 16, 20),
-            title: 'fifth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 14),
-            end: new Date(2022, 12, 16, 15),
-            title: 'sixth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 14),
-            end: new Date(2022, 12, 16, 15),
-            title: 'sixth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 14),
-            end: new Date(2022, 12, 16, 15),
-            title: 'sixth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 14),
-            end: new Date(2022, 12, 16, 15),
-            title: 'sixth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 14),
-            end: new Date(2022, 12, 16, 15),
-            title: 'sixth block'
-        },
-        {
-            start: new Date(2022, 12, 16, 10),
-            end: new Date(2022, 12, 16, 12, 30),
-            title: 'sixth block'
-        }
-    ];
+    let store: jasmine.SpyObj<Store>;
+    const mockTask: Task = {
+        title: 'title',
+        id: 1,
+        dashboardId: 1,
+        description: 'title',
+        taskCompletions: [
+            {
+                start: new Date(2022, 11, 16, 8),
+                end: new Date(2022, 13, 1, 0),
+                id: 1,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 8),
+                end: new Date(2022, 12, 22, 0),
+                id: 2,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 8),
+                end: new Date(2022, 12, 16, 11),
+                id: 3,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 15),
+                end: new Date(2022, 12, 16, 17),
+                id: 4,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 10),
+                end: new Date(2022, 12, 16, 23),
+                id: 5,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 12),
+                end: new Date(2022, 12, 16, 18),
+                id: 6,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 13),
+                end: new Date(2022, 12, 16, 20),
+                id: 7,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 14),
+                end: new Date(2022, 12, 16, 15),
+                id: 8,
+                taskId: 1,
+                isSuccessful: false
+            },
+            {
+                start: new Date(2022, 12, 16, 14),
+                end: new Date(2022, 12, 16, 15),
+                id: 9,
+                taskId: 1,
+                isSuccessful: false
+            }
+        ]
+    };
 
     beforeEach(async () => {
+        store = jasmine.createSpyObj('Store', ['select', 'dispatch']);
+        store.select.and.returnValue(of([mockTask]));
         await TestBed.configureTestingModule({
             declarations: [TimelineComponent, ExceedingTasksComponent],
             imports: [
@@ -89,16 +99,19 @@ describe('TimelineComponent', () => {
                 DashboardRoutingModule,
                 MaterialModule,
                 FormsModule
+            ],
+            providers: [
+                { provide: Store, useValue: store }
             ]
         })
             .compileComponents();
 
         fixture = TestBed.createComponent(TimelineComponent);
         component = fixture.componentInstance;
-        component.tasks = mockTasks;
         component.scale = TimelineScale.Day;
         component.date = new Date(2022, 12, 16, 12);
         fixture.detectChanges();
+        spyOn(component.blockClicked, 'emit');
     });
 
     it('should create', () => {
@@ -107,7 +120,7 @@ describe('TimelineComponent', () => {
 
     it('should contain 8 blocks when scale day', () => {
         let blocks = fixture.debugElement.queryAll(By.css('.task-block'));
-        expect(blocks.length === 8).toBeTrue();
+        expect(blocks.length === 6).toBeTrue();
     });
 
     it('should contain 5 blocks when scale week', () => {
@@ -129,5 +142,17 @@ describe('TimelineComponent', () => {
         fixture.detectChanges();
         let blocks = fixture.debugElement.queryAll(By.css('.task-block'));
         expect(blocks.length === 1).toBeTrue();
+    });
+
+    it('should call event when block clicked', () => {
+        component.scale = TimelineScale.Year;
+        fixture.detectChanges();
+
+        let block = fixture.debugElement.query(By.css('.task-block')).nativeElement;
+        block.click();
+        fixture.detectChanges();
+
+        expect(component.blockClicked.emit).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
     });
 });
