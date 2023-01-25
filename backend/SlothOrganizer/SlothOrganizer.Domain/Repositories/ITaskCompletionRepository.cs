5b409b8d3bad46a7f964e153ec6bfd6c05b959f9
Yurii Riabov <yurii.riabov@realplusonline.com>
2023-01-25 13:59:02 +0200

    Remove redundant namespace specification

--- backend/SlothOrganizer/SlothOrganizer.Domain/Repositories/ITaskCompletionRepository.cs
+++ backend/SlothOrganizer/SlothOrganizer.Domain/Repositories/ITaskCompletionRepository.cs
@@ -4,8 +4,8 @@ namespace SlothOrganizer.Domain.Repositories
 {
     public interface ITaskCompletionRepository
     {
-        System.Threading.Tasks.Task Delete(long taskId, DateTime repeatingEnd);
-        System.Threading.Tasks.Task Delete(long id);
+        Task Delete(long taskId, DateTime repeatingEnd);
+        Task Delete(long id);
         Task<List<TaskCompletion>> Insert(List<TaskCompletion> taskCompletions);
         Task<TaskCompletion?> Update(TaskCompletion taskCompletion);
     }
