47ea02eaf274ace2a20016c0f60f033e06098390
Yurii Riabov <yurii.riabov@realplusonline.com>
2023-01-25 13:52:15 +0200

    Remove accidentally added method

--- backend/SlothOrganizer/SlothOrganizer.Contracts/DTO/User/PasswordUpdateDto.cs
+++ backend/SlothOrganizer/SlothOrganizer.Contracts/DTO/User/PasswordUpdateDto.cs
@@ -5,10 +5,5 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string OldPassword { get; set; }
-
-        public bool IsValidPassword()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
