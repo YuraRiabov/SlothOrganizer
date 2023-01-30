f697809ed6d6cf46053919653ad7ad3ebae7ebda
Yurii Riabov <yurii.riabov@realplusonline.com>
2023-01-30 14:01:14 +0200

    Simplify task update result mapping with json

--- backend/SlothOrganizer/SlothOrganizer.Persistence/Properties/Resources.Designer.cs
+++ backend/SlothOrganizer/SlothOrganizer.Persistence/Properties/Resources.Designer.cs
@@ -283,11 +283,14 @@ namespace SlothOrganizer.Persistence.Properties {
         ///	[Description] = @Description
         ///WHERE Id = @Id
         ///
-        ///SELECT * 
-        ///FROM Tasks as t
-        ///INNER JOIN TaskCompletions AS tc
-        ///ON t.Id = tc.TaskId
-        ///WHERE t.Id = @Id.
+        ///SELECT TOP(1) *, (
+        ///	SELECT * 
+        ///	FROM TaskCompletions
+        ///	WHERE TaskId = @Id
+        ///	FOR JSON AUTO
+        ///) AS TaskCompletions
+        ///FROM Tasks
+        ///WHERE Id = @Id.
         /// </summary>
         internal static string UpdateTask {
             get {
--- backend/SlothOrganizer/SlothOrganizer.Persistence/Repositories/TaskRepository.cs
+++ backend/SlothOrganizer/SlothOrganizer.Persistence/Repositories/TaskRepository.cs
@@ -1,4 +1,6 @@
-﻿using Dapper;
+﻿using System.Text.Json.Serialization;
+using Dapper;
+using Newtonsoft.Json;
 using SlothOrganizer.Domain.Entities;
 using SlothOrganizer.Domain.Repositories;
 using SlothOrganizer.Persistence.Properties;
@@ -63,19 +65,16 @@ namespace SlothOrganizer.Persistence.Repositories
                 Description = task.Description,
             };
 
-            UserTask? updatedTask = null;
             var connection = _context.CreateConnection();
-            await connection.QueryAsync<UserTask, TaskCompletion, UserTask>(query, (task, completion) =>
+            var updatedTask = await connection.QueryAsync(query, parameters);
+            return updatedTask.Select(t => new UserTask
             {
-                if (updatedTask is null)
-                {
-                    updatedTask = task;
-                }
-                updatedTask.TaskCompletions.Add(completion);
-                return updatedTask;
-            }, 
-            param: parameters);
-            return updatedTask;
+                Id = t.Id,
+                Title = t.Title,
+                Description = t.Description,
+                DashboardId = t.DashboardId,
+                TaskCompletions = JsonConvert.DeserializeObject<List<TaskCompletion>>(t.TaskCompletions)
+            }).First();
         }
     }
 }
